23044b984b20ae688ca7171ef639ddbeef4a3520
tg0xff <tg0xff@proton.me>
2023-07-31 12:28:50 -0600

    Define capitaliseFirstLetter()

--- main.js
+++ main.js
@@ -13,6 +13,13 @@ function getComputerChoice() {
     }
 }
 
+function capitaliseFirstLetter(word) {
+    let letter = word.charAt(0);
+    let everythingElse = word.slice(1);
+    letter = letter.toUpperCase();
+    return letter + everythingElse;
+}
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
 
