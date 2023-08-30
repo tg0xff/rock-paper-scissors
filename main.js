b127da25d954bc233f162e884ea9ec99e1b46136
tg0xff <tg0xff@proton.me>
2023-08-29 19:15:12 -0600

    Keep track of rounds and show scores

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+let rounds = 0;
+let playerWins = 0;
+let cpuWins = 0;
+const resultText = document.querySelector("#result-text");
+const playerScore = document.querySelector("#player-score");
+const cpuScore = document.querySelector("#cpu-score");
+
 function getComputerChoice() {
   let choice = Math.random();
   choice = Number(choice.toFixed(4));
@@ -21,27 +28,55 @@ function capitaliseFirstLetter(word) {
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
 
-  let playerPrint = capitaliseFirstLetter(playerSelection);
-  let computerPrint = capitaliseFirstLetter(computerSelection);
-
+  // Return 1 if the player wins, 0 if the CPU wins, and -1 if it's a draw.
   if (playerSelection === computerSelection) {
-    return `Draw! ${playerPrint} vs ${computerPrint}.`;
+    return -1;
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return `You win! ${playerPrint} beats ${computerPrint}.`;
+    return 1;
   } else {
-    return `You lose! ${computerPrint} beats ${playerPrint}.`;
+    return 0;
   }
 }
 
 function game(playerSelection) {
   let computerSelection = getComputerChoice();
-  const p = document.querySelector("p");
+  let playerPrint = capitaliseFirstLetter(playerSelection);
+  let computerPrint = capitaliseFirstLetter(computerSelection);
+  let result;
 
-  p.textContent = playRound(playerSelection, computerSelection);
+  if (rounds < 5) {
+    result = playRound(playerSelection, computerSelection);
+
+    if (result === 1) {
+      resultText.textContent = `You win! ${playerPrint} beats ${computerPrint}.`;
+      playerWins++;
+    } else if (result === 0) {
+      resultText.textContent = `You lose! ${computerPrint} beats ${playerPrint}.`;
+      cpuWins++;
+    } else if (result === -1) {
+      resultText.textContent = `Draw! ${playerPrint} vs ${computerPrint}.`;
+      playerWins++;
+      cpuWins++;
+    }
+
+    playerScore.textContent = playerWins;
+    cpuScore.textContent = cpuWins;
+  } else if (rounds === 5) {
+    if (playerWins > cpuWins) {
+      resultText.textContent = "Player wins!";
+    } else if (cpuWins > playerWins) {
+      resultText.textContent = "CPU wins!";
+    } else if (playerWins === cpuWins) {
+      // Unlikely to happen, but worth considering nonetheless.
+      resultText.textContent = "It's a draw!";
+    }
+  }
+
+  rounds++;
 }
 
 const buttons = document.querySelectorAll("button");
