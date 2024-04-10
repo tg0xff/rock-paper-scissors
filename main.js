e2eabf6669e2ff1295fc5788fdfb7c1f28987e98
tg0xff <tg0xff@proton.me>
2024-04-10 15:17:22 -0600

    Write preliminary pseudo-code

---
+++ main.js
@@ -0,0 +1,18 @@
+// * Make a function that plays a round of rock paper scissors.
+// * Make a variable the stores the user's move.
+// * Make a variable the stores the CPU's move.
+// * Ask the user for input, so that they can make a move.
+// * Store the player's move in its variable.
+// * Normalise the user's input letter case, so that they can type their move
+// however they want.
+// * Ask for user input again if they didn't input a valid move.
+// * Generate a random number.
+// * Use that random number to determine the CPU's move.
+// * Store the CPU's move in its variable.
+// * Compare both variables and decide who's the winner according to the game's
+// rules.
+// * Print a sentence that tells the user who's the winner.
+// * Make a function that plays 5 rounds in a row.
+// * Print the scores after playing each round.
+// * Determine who's the winner after the 5 rounds have been played.
+// * Print a message that informs the user who's the overall winner.
