a477c9473da752ca94cd5033fbd43b8d63d2a43c
tg0xff <tg0xff@proton.me>
2024-04-12 21:12:06 -0600

    Move pseudo-code commments

--- main.js
+++ main.js
@@ -11,7 +11,12 @@ function capitalizeFirstLetter(s) {
 
 // * Make a function that plays 5 rounds in a row.
 function playGame(e) {
+  // * Ask the user for input, so that they can make a move.
+  // * Make a variable the stores the user's move.
+  // * Store the player's move in its variable.
   let playerMove = e.target.textContent;
+  // * Normalise the user's input letter case, so that they can type their move
+  // however they want.
   playerMove = playerMove.toLowerCase();
   let cpuMove = getComputerChoice();
   let roundResult = playRound(playerMove, cpuMove);
@@ -47,16 +52,11 @@ function playGame(e) {
   }
 }
 
-// * Ask the user for input, so that they can make a move.
 function getPlayerChoice() {
-  // * Make a variable the stores the user's move.
   let move;
   let keepGoing = true;
   while (keepGoing) {
-    // * Store the player's move in its variable.
     move = prompt("Enter your move, 'rock', 'paper' or 'scissors':");
-    // * Normalise the user's input letter case, so that they can type their move
-    // however they want.
     move = move.toLowerCase();
     // * Ask for user input again if they didn't input a valid move.
     if (move === "rock" || move === "paper" || move === "scissors") {
